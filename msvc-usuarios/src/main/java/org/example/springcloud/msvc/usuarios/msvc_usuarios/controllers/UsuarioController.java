d8bcf8202ba151660cb86679b6257b2d93a18f2b
Juan Carlos <juansuarezmarin@gmail.com>
2025-02-26 13:39:42 -0500

    #15 Rest controller, metodos post y put

--- msvc-usuarios/src/main/java/org/example/springcloud/msvc/usuarios/msvc_usuarios/controllers/UsuarioController.java
+++ msvc-usuarios/src/main/java/org/example/springcloud/msvc/usuarios/msvc_usuarios/controllers/UsuarioController.java
@@ -3,10 +3,9 @@ package org.example.springcloud.msvc.usuarios.msvc_usuarios.controllers;
 import org.example.springcloud.msvc.usuarios.msvc_usuarios.models.entity.Usuario;
 import org.example.springcloud.msvc.usuarios.msvc_usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,4 +30,21 @@ public class UsuarioController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id) {
+        Optional<Usuario> o = service.porId(id);
+        if (o.isPresent()) {
+            Usuario usuarioDb = o.get();
+            usuarioDb.setNombre(usuario.getNombre());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuario.getPassword());
+            return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
 }

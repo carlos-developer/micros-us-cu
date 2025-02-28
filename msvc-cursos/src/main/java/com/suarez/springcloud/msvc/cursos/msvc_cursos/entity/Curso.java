3b5d4c396424e7b4fe86faf80fff44fa9ef275dd
Juan Carlos <juansuarezmarin@gmail.com>
2025-02-28 09:59:18 -0500

    #22 Agragando la clase entity y el crud repository

---
+++ msvc-cursos/src/main/java/com/suarez/springcloud/msvc/cursos/msvc_cursos/entity/Curso.java
@@ -0,0 +1,32 @@
+package com.suarez.springcloud.msvc.cursos.msvc_cursos.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ msvc-cursos/src/main/java/com/suarez/springcloud/msvc/cursos/msvc_cursos/repositories/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.suarez.springcloud.msvc.cursos.msvc_cursos.repositories;
+
+import com.suarez.springcloud.msvc.cursos.msvc_cursos.entity.Curso;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CursoRepository extends CrudRepository<Curso, Long> {
+}

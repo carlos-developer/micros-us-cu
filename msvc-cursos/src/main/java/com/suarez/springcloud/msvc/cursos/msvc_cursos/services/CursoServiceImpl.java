1817c24a51efbe5ead07d969d0d00bbb34ad7740
Juan Carlos <juansuarezmarin@gmail.com>
2025-03-03 22:06:46 -0500

    #40 Msvc cursos detalle del curso con los alumnos asignados

--- msvc-cursos/src/main/java/com/suarez/springcloud/msvc/cursos/msvc_cursos/services/CursoServiceImpl.java
+++ msvc-cursos/src/main/java/com/suarez/springcloud/msvc/cursos/msvc_cursos/services/CursoServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CursoServiceImpl implements CursoService{
@@ -32,6 +33,23 @@ public class CursoServiceImpl implements CursoService{
     @Transactional(readOnly = true)
     public Optional<Curso> porId(Long id) {
         return repository.findById(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<Curso> porIdConUsuarios(Long id) {
+        Optional<Curso> o = repository.findById(id);
+        if(o.isPresent()) {
+            Curso curso = o.get();
+            if(!curso.getCursoUsuarios().isEmpty()) {
+                List<Long> ids = curso.getCursoUsuarios().stream().map(CursoUsuario::getUsuarioId).toList();
+
+                List<Usuario> usuarios = client.obtenerALumnosPOrCurso(ids);
+                curso.setUsuarios(usuarios);
+            }
+            return Optional.of(curso);
+        }
+        return Optional.empty();
     }
 
     @Override
